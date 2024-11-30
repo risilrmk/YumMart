1e266dff5858e76897a13ad32a4245f45f6d4d17
risilmrk <risilbinmusthafa@gmail.com>
2024-11-30 12:27:56 +0530

    Your commit message here

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom';
 
 
 
-const Navbar = () => {
+const Navbar = ({setShowLogin} ) => {
     const [menu,setMenu] = useState("home");
     return (
         <div>
             <div className='navbar'>
                 <img src={assets.logo} alt="Website Logo" className='logo' />
                 <ul className="navbar-menu">
-                    <li><a onClick={()=>setMenu("home")} className={menu==="home"?"active":""} href="#home">Home</a></li>
-                    <li><a onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}href="#menu">Menu</a></li>
-                    <li><a onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}href="#mobile-app">Mobile App</a></li>
-                    <li><a onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}href="#contact-us">Contact Us</a></li>
+                    <Link><a onClick={()=>setMenu("home")} className={menu==="home"?"active":""} href="#home">Home</a></Link>
+                    <li><a onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}href="#explore-menu">Menu</a></li>
+                    <li><a onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}href="#app-download">Mobile App</a></li>
+                    <li><a onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}href="#footer">Contact Us</a></li>
                 </ul>
                 <div className="navbar-right">
                     <img src={assets.search_icon} alt="Search Icon" className="search-icon" />
@@ -23,7 +23,7 @@ const Navbar = () => {
                         <Link to="/cart"><img  src={assets.basket_icon} alt="Basket Icon" /></Link>
                         <div className="dot"></div>
                     </div>
-                    <button aria-label="Sign in">Sign in</button>
+                    <button onClick={()=>setShowLogin(true)}>Sign in</button>
                 </div>
             </div>
         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
 import './Home.css'
-import Header from '../../Components/Header/Header'
-import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../Components/AppDownload/AppDownload'
 
 const Home = () => {
-  const  [category,setCategory] = useState("All")
+
+  const [category,setCategory] = useState("All");
+
   return (
     <div>
-
       <Header/>
-      <ExploreMenu category={category} setCategory={setCategory} />
-      <FoodDisplay/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category} setCategory={setCategory}/>
+      <AppDownload/>
     </div>
   )
 }
