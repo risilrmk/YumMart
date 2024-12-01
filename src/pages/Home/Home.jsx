4ca6da978144147b652c4f1bcd4df538cf2557bb
Muhammed Risil <87575552+risilrmk@users.noreply.github.com>
2024-12-01 12:22:00 +0530

    Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
+
 import AppDownload from '../../Components/AppDownload/AppDownload'
 
 
