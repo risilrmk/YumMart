633bf5b2290583040aa0c195ef66aca575dc6504
risilmrk <risilbinmusthafa@gmail.com>
2024-12-03 07:10:42 +0530

    finished

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
+
 
 const PlaceOrder = () => {
+
+const {getTotalCartAmount} = useContext(StoreContext)
+
   return (
-    <div>
-      
+    <div className='place-order'>
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name' />
+        </div>
+        <input type="text" placeholder='Email address' />
+        <input type="text" placeholder='Street' />
+        <div className="multi-fields">
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
+        </div><div className="multi-fields">
+          <input type="text" placeholder='Pincode' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type="text" placeholder='Phone number' />
+      </div>
+      <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>₹{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery fee</p>
+              <p>₹{getTotalCartAmount()===0?0:49}</p>
+
+
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>₹{getTotalCartAmount()===0?0:getTotalCartAmount()+49}</b>
+            </div>
+          </div>
+          <button>PROCEED TO PAY</button>
+        </div>
+      </div>
     </div>
   )
 }
